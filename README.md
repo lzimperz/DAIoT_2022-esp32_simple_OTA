8662e551e020ee066963fee86d279d58c7880b8a
Leopoldo A. Zimperz <leopoldo.a.zimperz@gmail.com>
2022-05-09 18:36:53 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Simple OTA example
+
+This example is based on `esp_https_ota` component's APIs.
+
+## Configuration
+
---
+++ main/simple_ota.c
@@ -0,0 +1,187 @@
+/* DAIoT 2022 OTA example */
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_ota_ops.h"
+#include "esp_http_client.h"
+#include "esp_https_ota.h"
+#include "protocol_examples_common.h"
+#include "string.h"
+
+#include "nvs.h"
+#include "nvs_flash.h"
+#include <sys/socket.h>
+#include "esp_wifi.h"
+
+
+#define HASH_LEN 32
+
+
+/* -------- EXAMPLE BASIC CONFIGURATIONS start ------------------------------------------------- */
+
+/* Set firmware update server URL and FILENAME */
+#define DAIOT_SIMPLE_OTA_FIRMWARE_UPGRADE_URL   "https://192.168.68.54:8070/DAIoT_simple_ota.bin"
+
+/* The interface name value can refer to if_desc in esp_netif_defaults.h */
+/* Select network interface (uncomment correct interface declaration) */
+static const char *bind_interface_name = "sta";     // Wi-Fi interface
+// static const char *bind_interface_name = "eth";  // Ethernet interface
+
+/* Wi-Fi SSID and Wi-Fi PASSWORD can be set up on the project configuration.
+      You can run "idf.py menuconfig" in a terminal 
+      or tap the config icon in the VSCode toolbar                            */
+
+/* -------- EXAMPLE BASIC CONFIGURATIONS end --------------------------------------------------- */
+
+static const char *TAG = "DAIoT_simple_OTA";
+extern const uint8_t server_cert_pem_start[] asm("_binary_ca_cert_pem_start");
+extern const uint8_t server_cert_pem_end[] asm("_binary_ca_cert_pem_end");
+
+#define OTA_URL_SIZE 256
+
+esp_err_t _http_event_handler(esp_http_client_event_t *evt)
+{
+    switch (evt->event_id) {
+    case HTTP_EVENT_ERROR:
+        ESP_LOGD(TAG, "HTTP_EVENT_ERROR");
+        break;
+    case HTTP_EVENT_ON_CONNECTED:
+        ESP_LOGD(TAG, "HTTP_EVENT_ON_CONNECTED");
+        break;
+    case HTTP_EVENT_HEADER_SENT:
+        ESP_LOGD(TAG, "HTTP_EVENT_HEADER_SENT");
+        break;
+    case HTTP_EVENT_ON_HEADER:
+        ESP_LOGD(TAG, "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
+        break;
+    case HTTP_EVENT_ON_DATA:
+        ESP_LOGD(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+        break;
+    case HTTP_EVENT_ON_FINISH:
+        ESP_LOGD(TAG, "HTTP_EVENT_ON_FINISH");
+        break;
+    case HTTP_EVENT_DISCONNECTED:
+        ESP_LOGD(TAG, "HTTP_EVENT_DISCONNECTED");
+        break;
+    }
+    return ESP_OK;
+}
+
+void simple_ota_example_task(void *pvParameter)
+{
+    ESP_LOGI(TAG, "Starting OTA example");
+
+    esp_netif_t *netif = get_example_netif_from_desc(bind_interface_name);
+    if (netif == NULL) {
+        ESP_LOGE(TAG, "Can't find netif from interface description");
+        abort();
+    }
+    struct ifreq ifr;
+    esp_netif_get_netif_impl_name(netif, ifr.ifr_name);
+    ESP_LOGI(TAG, "Bind interface name is %s", ifr.ifr_name);
+
+    esp_http_client_config_t config = {
+        .url = DAIOT_SIMPLE_OTA_FIRMWARE_UPGRADE_URL,
+        .cert_pem = (char *)server_cert_pem_start,
+        .event_handler = _http_event_handler,
+        .keep_alive_enable = true,
+        .if_name = &ifr,
+    };
+
+
+    // Skip CERT COMMON NAME CHECK - Only for testing
+    config.skip_cert_common_name_check = true;
+
+    esp_err_t ret = esp_https_ota(&config);
+    if (ret == ESP_OK) {
+
+        ESP_LOGI(TAG, "OTA Tool Example v1.0");
+
+        const esp_partition_t *running = esp_ota_get_running_partition();
+
+        // Display the running partition
+        ESP_LOGI(TAG, "Running partition: %s", running->label);
+
+        ESP_LOGI(TAG, "Example end");
+
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+        ESP_LOGI(TAG, "10 seconds to restart and update again... (unuseless, only for test)");
+
+        vTaskDelay(10000 / portTICK_PERIOD_MS);
+
+        ESP_LOGI(TAG, "Restarting...");
+
+        esp_restart();
+    } else {
+        ESP_LOGE(TAG, "Firmware upgrade failed");
+    }
+    while (1) {
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
+static void print_sha256(const uint8_t *image_hash, const char *label)
+{
+    char hash_print[HASH_LEN * 2 + 1];
+    hash_print[HASH_LEN * 2] = 0;
+    for (int i = 0; i < HASH_LEN; ++i) {
+        sprintf(&hash_print[i * 2], "%02x", image_hash[i]);
+    }
+    ESP_LOGI(TAG, "%s %s", label, hash_print);
+}
+
+static void get_sha256_of_partitions(void)
+{
+    uint8_t sha_256[HASH_LEN] = { 0 };
+    esp_partition_t partition;
+
+    // get sha256 digest for bootloader
+    partition.address   = ESP_BOOTLOADER_OFFSET;
+    partition.size      = ESP_PARTITION_TABLE_OFFSET;
+    partition.type      = ESP_PARTITION_TYPE_APP;
+    esp_partition_get_sha256(&partition, sha_256);
+    print_sha256(sha_256, "SHA-256 for bootloader: ");
+
+    // get sha256 digest for running partition
+    esp_partition_get_sha256(esp_ota_get_running_partition(), sha_256);
+    print_sha256(sha_256, "SHA-256 for current firmware: ");
+}
+
+void app_main(void)
+{
+    // Initialize NVS.
+    esp_err_t err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        // 1.OTA app partition table has a smaller NVS partition size than the non-OTA
+        // partition table. This size mismatch may cause NVS initialization to fail.
+        // 2.NVS partition contains data in new format and cannot be recognized by this version of code.
+        // If this happens, we erase NVS partition and initialize NVS again.
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        err = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(err);
+
+    get_sha256_of_partitions();
+
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+     * Read "Establishing Wi-Fi or Ethernet Connection" section in
+     * examples/protocols/README.md for more information about this function.
+     */
+    ESP_ERROR_CHECK(example_connect());
+
+    /* Ensure to disable any WiFi power save mode, this allows best throughput
+     * and hence timings for overall OTA operation.
+     */
+    esp_wifi_set_ps(WIFI_PS_NONE);
+
+    /* Starts DAIoT Simple OTA task.
+     */
+    xTaskCreate(&simple_ota_example_task, "ota_example_task", 8192, NULL, 5, NULL);
+}
